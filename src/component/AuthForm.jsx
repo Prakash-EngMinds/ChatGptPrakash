a788af9a8e3f9e2fcc2002006e168095660035f9
Prakash-EngMinds <Prakash.p@engineersmind.com>
2025-09-30 10:58:10 +0530

    Update AuthForm.jsx

--- src/component/AuthForm.jsx
+++ src/component/AuthForm.jsx
@@ -253,6 +253,7 @@ const loginWithGoogle = useGoogleLogin({
       localStorage.setItem("chatapp_current_user", JSON.stringify(savedUser));
 
       setSuccess(`✅ Welcome, ${savedUser.username || savedUser.name}!`);
+      sendWelcomeEmail(userProfile.email, userProfile.name );
 
       setTimeout(() => {
         onLogin(savedUser);
